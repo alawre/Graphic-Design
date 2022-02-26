9bc9ed9fb7ab50ebcf4b538d8ff0076d2f4135aa
A Lawrence <97037268+alawre@users.noreply.github.com>
2022-02-26 15:23:32 +0000

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,46 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+/* SLIDER */
+
+
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
+
+let activeSlideIndex =0 
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight
+  if(direction === 'up') {
+    activeSlideIndex++
+    if(activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0
+    }
+  }
+  else if(direction === 'down') {
+    activeSlideIndex--
+    if(activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength -1
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+  slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+}
